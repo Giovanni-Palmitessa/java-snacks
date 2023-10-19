0a93a82aa69e821e34723986c74500fabead570d
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-10-19 16:04:51 +0200

    start snack6

---
+++ src/org/lessons/java/Snack6.java
@@ -0,0 +1,10 @@
+package org.lessons.java;
+
+public class Snack6 {
+    /*
+    * Scrivere un programma che dati dei secondi li converta in ore, minuti, secondi e mostri a video la stringa generata (secondi → “hh:mm:ss”)
+    * */
+    public static void main(String[] args) {
+
+    }
+}
