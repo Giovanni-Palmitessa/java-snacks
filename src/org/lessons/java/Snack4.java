80e6a151fe83a2376172e0b654cb2b95c36d183e
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-10-19 15:02:34 +0200

    add scanner and class snack 4

---
+++ src/org/lessons/java/Snack4.java
@@ -0,0 +1,14 @@
+package org.lessons.java;
+
+import java.util.Scanner;
+
+public class Snack4 {
+    /*
+    * Data in input una stringa verificare se è palindroma
+    * */
+    public static void main(String[] args) {
+        //istanzio lo scanner
+        Scanner scan = new Scanner(System.in);
+
+    }
+}
