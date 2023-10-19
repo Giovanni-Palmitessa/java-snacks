1f24bb1e81a1321ee713a6d21fcea6eefec53a33
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-10-19 14:20:30 +0200

    Add comments and close scan

--- src/org/lessons/java/Snack1.java
+++ src/org/lessons/java/Snack1.java
@@ -12,19 +12,25 @@ public class Snack1 {
         Scanner scan = new Scanner(System.in);
 
         while (true) {
+            //chiedo all'utente di inserire un numero
             System.out.print("Inserisci un numero: ");
             int numero = scan.nextInt();
 
+            // se numero minore di 0 interrompo il ciclo
             if (numero < 0) {
                 System.out.println("Hai inserito un numero negativo. Game Over!");
                 break; // Esci dal ciclo se il numero è negativo
             }
 
+            // stampo il numero scritto
             if (numero % 2 == 0) {
                 System.out.println("Il numero " + numero + " è pari.");
             } else {
                 System.out.println("Il numero " + numero + " è dispari, il successivo è " + (numero + 1) + ".");
             }
+
+            // chiudo lo scanner
+            scan.close();
         }
     }
 }
