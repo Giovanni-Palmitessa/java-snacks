082d342e7d2423a180876c9b3a5481c883e4dc55
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-10-19 15:04:25 +0200

    add input for ask word

--- src/org/lessons/java/Snack4.java
+++ src/org/lessons/java/Snack4.java
@@ -10,5 +10,9 @@ public class Snack4 {
         //istanzio lo scanner
         Scanner scan = new Scanner(System.in);
 
+        //chiedo all'utente una parola
+        System.out.println("Dammi una parola e ti dirò se è palindroma: ");
+        String word = scan.nextLine();
+
     }
 }
